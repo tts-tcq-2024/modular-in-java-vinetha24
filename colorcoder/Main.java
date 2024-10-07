1c25a9d813f63057f56a21858c6af3c39491a96c
vinetha24 <113199878+vinetha24@users.noreply.github.com>
2024-10-07 09:10:30 +0530

    Update Main.java

--- colorcoder/Main.java
+++ colorcoder/Main.java
@@ -3,26 +3,26 @@ package colorcoder;
 
 import java.util.Scanner;
 public class Main {
-      final static String MajorColorNames[] = {
+      final static String MAJOR_COLORS[] = {
           "White", "Red", "Black", "Yellow", "Violet"
       };      
-      final static int numberOfMajorColors = MajorColorNames.length;    
-      final static String MinorColorNames[] = {
+      final static int NUMBER_OF_MAJORCOLOR = MAJOR_COLORS.length;    
+      final static String MINOR_COLORS[] = {
           "Blue", "Orange", "Green", "Brown", "Slate"
       };     
-      final static int numberOfMinorColors = MinorColorNames.length;
+      final static int NUMBER_OF_MINORCOLOR = MINOR_COLORS.length;
       
       public static ColorPair getColorFromPairNumber(int pairNumber) {
           int zeroBasedPairNumber = pairNumber - 1;
           MajorColor majorColor = 
-              IColor.fromIndex(MajorColor.class,zeroBasedPairNumber / numberOfMinorColors);
+              IColor.fromIndex(MajorColor.class,zeroBasedPairNumber / NUMBER_OF_MINORCOLOR);
           MinorColor minorColor =
-              IColor.fromIndex(MinorColor.class,zeroBasedPairNumber % numberOfMinorColors);
+              IColor.fromIndex(MinorColor.class,zeroBasedPairNumber % NUMBER_OF_MINORCOLOR);
           return new ColorPair(majorColor, minorColor);
       }
       
-      public static int getPairNumberFromColor(MajorColor majorColor, MinorColor minorColor) {
-          return majorColor.getIndex() * numberOfMinorColors + minorColor.getIndex() + 1;
+      public static int getPairNumberFromColor(MajorColor major, MinorColor minor) {
+          return major.getIndex() * NUMBER_OF_MINORCOLOR + minor.getIndex() + 1;
       }  
 
       public static void main(String[] args) {
@@ -31,9 +31,11 @@ public class Main {
         System.out.println("Enter the Major color : ");
         String majorColor = inputFromUser.nextLine().trim().toUpperCase();
         System.out.println("Enter the Minor color : ");
-        String minorColor = inputFromUser.nextLine().trim().toUpperCase();       
+        String minorColor = inputFromUser.nextLine().trim().toUpperCase(); 
+        if(MAJOR_COLORS.toString().contains(majorColor)){
         int pairNumber = (getPairNumberFromColor(MajorColor.valueOf(majorColor), MinorColor.valueOf(minorColor)));
         System.out.println("ColorCode is : " + pairNumber);
+        }
       }
   }
 
